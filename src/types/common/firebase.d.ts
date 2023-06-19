985419acb4022cdd1a3d833bb7646dc459155c7e
Jan Mikulka <jan.mikulka@email.cz>
2023-06-19 09:40:44 +0200

    Add general firestore types

--- src/types/common/firebase.d.ts
+++ src/types/common/firebase.d.ts
@@ -1,2 +1,17 @@
 /** value being written to the Firestore database cannnot be undefined */
-export type AnyFirestoreValue = {} | null;
+export type AnyFirestoreValue = object | null;
+
+const a: AnyFirestoreValue = 
+
+a
+
+export interface FirestoreDocument {
+  id: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export interface FirestoreDocumentUpdate {
+  updatedAt: Date;
+  [key: string]: AnyFirestoreValue;
+}
